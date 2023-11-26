402a397e83ae91c2edfea17bae5783efada123cd
Tutoriais por Max Vianna <tutoriaismaxvianna@gmail.com>
2023-11-26 17:08:51 -0300

    Usando o Helper do CakePHP para visualizar as imgs

--- templates/element/adminlte/preloader.php
+++ templates/element/adminlte/preloader.php
@@ -1,4 +1,8 @@
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+      <?= $this->Html->image('adminlte/AdminLTELogo.png', [
+            'alt' => 'AdminLTELogo',
+            'height' => '60',
+            'width' => '60'
+        ]); ?>
   </div>
